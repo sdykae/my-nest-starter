e7b35bc8a56e91203dcf935c77bf38a1fa19331d
iuslkae <iuslkae@protonmail.com>
2021-04-05 21:54:48 -0500

    clean

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,9 +16,19 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier':'error',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type':'off'
   },
+  overrides: [
+    {
+      'files':['*.ts'],
+      'rules':{
+        '@typescript-eslint/explicit-function-return-type':['error']
+      }
+    }
+  ]
 };
